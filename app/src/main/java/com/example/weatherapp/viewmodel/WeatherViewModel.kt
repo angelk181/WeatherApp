0117b779dfc15338c32d97dd47aae073d3ff02c9
angelk181 <angel.keene64@gmail.com>
2021-07-13 21:34:31 +0100

    Added observers for forecast views

--- app/src/main/java/com/example/weatherapp/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/viewmodel/WeatherViewModel.kt
@@ -10,6 +10,8 @@ import com.example.weatherapp.model.Weather
 import com.example.weatherapp.repository.WeatherRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.time.DayOfWeek
+import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,8 +25,38 @@ constructor(private val repository: WeatherRepository) : ViewModel() {
     val weatherResponse: LiveData<Weather>
     get() = _response
 
+    val dayOfWeek: DayOfWeek = LocalDate.now().dayOfWeek
+    val dayNum: Int = dayOfWeek.value
+
+    val daysMap = mapOf(
+        0 to "Sunday",
+        1 to "Monday",
+        2 to "Tuesday",
+        3 to "Wednesday",
+        4 to "Thursday",
+        5 to "Friday",
+        6 to "Saturday"
+    )
+
+
+    val day1: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val day2: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val day3: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+
+
+
+
+
     init {
         getWeather()
+
     }
 
 
@@ -41,6 +73,13 @@ constructor(private val repository: WeatherRepository) : ViewModel() {
             }
         }
     }
+
+    private fun initForecast() {
+
+
+    }
+
+
 
 
 }
